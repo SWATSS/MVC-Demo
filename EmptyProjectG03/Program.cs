a3c4f67a4c62d7f9ad6c4d5a7e9d6694afd7f695
fox <textfox10000@gmail.com>
2025-09-05 01:21:33 +0300

    01

--- EmptyProjectG03/Program.cs
+++ EmptyProjectG03/Program.cs
@@ -4,6 +4,45 @@ namespace EmptyProjectG03
     {
         public static void Main(string[] args)
         {
+            #region Notes
+
+            //MVC Pattern:
+            //- Model
+            //- View
+            //- Controller
+
+            //Types of Applications:
+            //- MPA (Multiple Page Application)
+            //- SPA (Single Page Application)
+            //- RESTful Services
+            //- Real-time Apps
+
+            //HTTP Protocol:
+            //- Request
+            //- Response
+
+            //Important Protocols:
+            //- HTTP
+            //- FTP
+            //- TCP
+
+            //URL : Uniform Resource Locator
+
+            //Server & Hosting:
+            //- IIS
+            //- Apache / Nginx
+            //- Kestrel
+            //- Hosting Models:
+            //  - In-Process
+            //  - Out-of-Process
+
+            //Routing:
+            //- Conventional Routing
+            //- Attribute Routing
+            //- Route Constraints
+
+            #endregion
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Services
@@ -11,7 +50,7 @@ namespace EmptyProjectG03
 
             var app = builder.Build();
 
-            
+
 
             if (app.Environment.IsDevelopment())
             {
